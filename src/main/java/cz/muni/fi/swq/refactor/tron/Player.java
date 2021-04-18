22cb687de40ecb96fae759f401052e097d7752d1
Adam Ruman <469068@muni.cz>
2021-04-18 18:04:45 +0200

    adds direction to players

--- src/main/java/cz/muni/fi/swq/refactor/tron/Player.java
+++ src/main/java/cz/muni/fi/swq/refactor/tron/Player.java
@@ -1,20 +1,24 @@
 package cz.muni.fi.swq.refactor.tron;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
 
     private Position position;
+    private Direction direction;
     private List<Position.Coordinate> path;
 
     Player(
             int startX,
             int startY,
             int maxX,
-            int maxY ) {
+            int maxY,
+            Direction direction) {
 
         position = new Position(startX, startY, maxX, maxY);
-
+        this.direction = direction;
+        path = new ArrayList<>();
     }
 
 }
