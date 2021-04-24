fe67150c1dea4372e369ff03ef1b83674cd4d4d2
Andrej Tomči <469192@mail.muni.cz>
2021-04-24 16:18:20 +0200

    Create PlayGround interface

---
+++ src/main/java/cz/muni/fi/swq/refactor/tron/engine/PlayGroundContract.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.swq.refactor.tron.engine;
+
+import java.util.HashMap;
+import java.awt.Color;
+import java.util.List;
+
+/**
+ * @author Andrej Tomci
+ */
+public interface PlayGroundContract {
+    void gameTick();
+    List<ColoredRectangle> getGraphicObjects();
+}
